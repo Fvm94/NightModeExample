35b3b3586ac553c3f2d5c53671c7f014c76d54ca
Franc <franciscovasquesmendes@gmail.com>
2020-11-12 00:18:57 -0300

    Clean code

--- app/src/main/java/com/franciscovm/nightmodeexample/MainActivity.kt
+++ app/src/main/java/com/franciscovm/nightmodeexample/MainActivity.kt
@@ -1,44 +1,91 @@
 package com.franciscovm.nightmodeexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.widget.RadioButton
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNightModeID = AppCompatDelegate.MODE_NIGHT_UNSPECIFIED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-    }
 
-    fun onRadioButtonClicked(view: View) {
-        if (view is RadioButton) {
-            // Is the button now checked?
-            val checked = view.isChecked
 
-            var modeNight = 0
+        //GetSharedPreferences
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        currentNightModeID = sharedPref.getInt(getString(R.string.saved_night_mode_key),AppCompatDelegate.MODE_NIGHT_UNSPECIFIED)
 
-            // Check which radio button was clicked
-            when (view.getId()) {
-                R.id.main_night_on_rb ->
-                    if (checked) {
-                        modeNight = AppCompatDelegate.MODE_NIGHT_YES
-                    }
-                R.id.main_night_off_rb ->
-                    if (checked) {
-                        modeNight = AppCompatDelegate.MODE_NIGHT_NO
-                    }
-                R.id.main_night_auto_rb ->
-                    if (checked){
-                        modeNight = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-                    }
-            }
+        setNightMode(currentNightModeID)
 
-            AppCompatDelegate.setDefaultNightMode(modeNight)
-
+        val button = findViewById<Button>(R.id.main_theme_bt)
+        button.setOnClickListener {
+            showDialog()
         }
     }
 
+    private fun convertedCurrentNightMode(): Int {
+
+        val nightMode = currentNightModeID
+        var i = AppCompatDelegate.MODE_NIGHT_UNSPECIFIED
+
+        when (nightMode) {
+            AppCompatDelegate.MODE_NIGHT_NO ->
+                i = 0
+            AppCompatDelegate.MODE_NIGHT_YES ->
+                i = 1
+        }
+        return i
+    }
+
+    private fun showDialog() {
+
+        val builder = AlertDialog.Builder(this)
+        var selectedMode = convertedCurrentNightMode()
+
+        builder.setTitle(getString(R.string.choose_theme))
+                .setSingleChoiceItems(R.array.themes,
+                        selectedMode
+                ) {_,  which ->
+                    selectedMode = which
+                }.setPositiveButton(getString(R.string.ok)) {_,_->
+                    changeTheme(selectedMode)
+                    Log.i(TAG, "Positive pressed")
+                }.setNegativeButton(getString(R.string.cancel)) { _, _ ->
+                    Log.i(TAG, "Negative pressed")
+                }.create().show()
+    }
+
+    private fun changeTheme(selectedMode: Int) {
+
+        var nightMode = AppCompatDelegate.MODE_NIGHT_UNSPECIFIED
+
+        when (selectedMode) {
+            0 ->
+                nightMode = AppCompatDelegate.MODE_NIGHT_NO
+            1 ->
+                nightMode = AppCompatDelegate.MODE_NIGHT_YES
+        }
+
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putInt(getString(R.string.saved_night_mode_key), nightMode)
+            commit()
+        }
+        setNightMode(nightMode)
+    }
+
+    private fun setNightMode(id:Int){
+        AppCompatDelegate.setDefaultNightMode(id)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity.Inf"
+    }
 }
+
